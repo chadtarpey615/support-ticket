f4587347d1800e819c2e882bdaed563bce84f016
Chad Tarpey <chadtarpey@gmail.com>
2022-02-10 17:42:12 -0600

    created user model and can register new users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler")
+const bcrypt = require("bcryptjs")
+const User = require("../models/userModel")
 // @desc regster new user
 // @route /api/user
 // @access public
@@ -10,7 +12,39 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("Please include all fields")
     }
-    res.send("Register Route")
+
+    // find if user exists
+    const userExists = await User.findOne({ email })
+
+    if (userExists)
+    {
+        res.status(400)
+        throw new Error("User already exists")
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create new user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user)
+    {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else
+    {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
 })
 
 // @desc login user
