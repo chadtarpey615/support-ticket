c323d0e6ab9f1f8815413dcde9e876ef71ef8b98
Chad Tarpey <chadtarpey@gmail.com>
2022-02-21 14:12:48 -0600

    created note model, routes, and controller

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const router = express.Router()
+
 const { protect } = require("../middleware/authMiddleware")
 const { getTickets, getUserTicket, createTicket, deleteTicket, updateTicket } = require("../controllers/ticketController")
+
+
+// re route into note router
+const noteRouter = require("./noteRoutes")
+router.use("/:ticketId/notes", noteRouter)
+
 
 router.route("/").get(protect, getTickets).post(protect, createTicket)
 
